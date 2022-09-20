165e3ecf965c35a95de087a8b6008d6dd0691c63
egjallo <egjallo@insistglobal.com>
2022-09-20 15:34:25 +0000

    test1

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+the "0-add_integer" module
+
+The 0-add_integer module supplies one function, add_integer(). For example,
+
+>>> add_integer(13, 29)
+42
+"""
+
+
+def add_integer(a, b=98):
+    """ Returns the sum of two int or float parameters. """
+    if not type(a) == int:
+        if type(a) == float:
+            a = int(a)
+        else:
+            raise TypeError("a must be an integer")
+    if not type(b) == int:
+        if type(b) == float:
+            b = int(b)
+        else:
+            raise TypeError("b must be an integer")
+    return a + b
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
