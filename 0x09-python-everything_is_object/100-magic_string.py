b18e7da4943ca127115015dc994c4040e4a17d25
egjallow10 <egjallow10@gmail.com>
2022-09-20 16:54:56 -0700

    magicc

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-
-
-def call_counter(func):
-    def helper():
-        helper.calls += 1
-        return func()
-    helper.calls = 0
-
-    return helper
-
-
-@call_counter
-def magic_string():
-    return 'Holberton, ' * (magic_string.calls - 1) + 'Holberton'
+def magic_string(static_list=[]):
+    static_list.append("Holberton")
+    return ', '.join(static_list)
