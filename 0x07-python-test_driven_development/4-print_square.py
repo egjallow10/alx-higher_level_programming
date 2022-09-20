6ad18d0b373cf32f8e346804f21df4b7fab64d1e
egjallo <egjallo@insistglobal.com>
2022-09-20 16:00:24 +0000

    square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This is the "4-print_square" module
+The 4-print_square module supplies one function, matrix_divided().
+For example,
+
+>>> print_square(4)
+####
+####
+####
+####
+
+"""
+
+
+def print_square(size):
+    """ Prints a square made with the char `#`. """
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif size > 0:
+        print(('#' * size + '\n') * size, end="")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
