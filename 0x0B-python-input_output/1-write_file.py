7749076a47e680c2fb3cbbcb3215e3f19bdce34b
egjallo <93976165+egjallo@users.noreply.github.com>
2022-09-27 18:59:10 -0700

    triangle

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-""" module `1-number_of_lines` provides the `number_of_lines` function """
+""" module  provides the `number_of_lines` function """
 
 
-def number_of_lines(filename=""):
-    """ returns the number of lines of a text file """
-    with open(filename, 'r', encoding='utf8') as f:
-        return sum(1 for line in f)
-
-
-if __name__ == '__main__':
-
-    filename = "my_file_0.txt"
-    nb_lines = number_of_lines(filename)
-    print("{} has {:d} lines".format(filename, nb_lines))
+def write_file(filename="", text=""):
+    """ returns the number of chars written to `filename` from `text` """
+    with open(filename, 'w', encoding='utf=8') as f:
+        return f.write(text)
