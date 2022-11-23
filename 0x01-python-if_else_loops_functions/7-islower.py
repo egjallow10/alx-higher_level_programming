2be91fe8681ff9f7280a4004c55ba34357a10f0b
egjallo <egjallo@insistglobal.com>
2022-11-23 16:16:41 +0000

    fixed

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def uppercase(str):
-    tmp = list(str)
-    for i in range(len(tmp)):
-        if (ord(tmp[i]) > 96 and ord(tmp[i]) < 123):
-            tmp[i] = chr(ord(tmp[i]) - 32)
-    print("{}".format("".join(tmp)))
+def islower(c):
+    return ord(c) == 97 and ord(c) <= 122
