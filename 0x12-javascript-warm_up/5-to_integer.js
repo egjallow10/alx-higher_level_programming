3e20e6a8b455a7817282e99d930328b3584add65
egjallo <egjallow10@gmail.com>
2022-12-01 00:02:10 +0000

    argumet numbera

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
-  console.log('Not a number');
-} else {
-  console.log(`My number: ${parseInt(process.argv[2])}`);
-}
+const n = parseInt(process.argv[2]);
+console.log(isNaN(n) ? 'Not a number' : `My number: ${n}`);
