a884ef8a14c6af15767fc356b4098db99e069e47
egjallo <egjallow10@gmail.com>
2022-12-02 21:04:16 +0000

    print square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 const times = parseInt(process.argv[2]);
-if (times) {
+if (times < 0) {
+  return
+}else{
   for (const ch of 'X'.repeat(times)) {
     console.log(ch.repeat(times));
   }
