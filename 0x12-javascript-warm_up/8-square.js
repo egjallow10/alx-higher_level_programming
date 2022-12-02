affeeeb79c3eda9ce844676bd4d33df7b997f538
egjallo <egjallow10@gmail.com>
2022-12-02 20:59:09 +0000

    print square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const times = parseInt(process.argv[2]);
-if (isNaN(times)) {
-  console.log('Missing size');
-} else if (times > 0) {
+if (times) {
   for (const ch of 'X'.repeat(times)) {
     console.log(ch.repeat(times));
   }
