4992a7e55cec146de36f343794e5dbc056d3d57b
egjallo <egjallow10@gmail.com>
2022-12-02 21:23:00 +0000

    change

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 const times = parseInt(process.argv[2]);
-if (times < 0) {
-  return
-}else{
-  for (const ch of 'X'.repeat(times)) {
-    console.log(ch.repeat(times));
+if (isNaN(times)) {
+  console.log('Missing size');
+} else if (times > 0) {
+  for (let i = 0; i < times; i++) {
+    let value = '';
+    for (let j = 0; j < times; j++) {
+      value += 'X';
+    }
+    console.log(value);
   }
 }
