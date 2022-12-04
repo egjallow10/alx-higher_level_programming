a0f7806a169cf385cb0a3492c782525851302a17
egjallo <egjallow10@gmail.com>
2022-12-04 08:06:47 +0000

    print state where name = arg is safeway

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""List all states where state == arg from the database"""
+
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+
+    name = argv[4]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+    cursor = db.cursor()
+    cursor.execute(("SELECT * FROM states "
+                    "WHERE name= %s"
+                    "ORDER BY id"), name)
+    states = cursor.fetchall()
+    for state in states:
+        if state[1] == name:
+            print(state)
+    cursor.close()
+    db.close()
