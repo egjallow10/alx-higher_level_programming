a3546020adc319d62ff4245f776d1c62a8e1f10c
egjallo <egjallow10@gmail.com>
2022-12-04 12:17:35 +0000

    changes

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -19,6 +19,6 @@ if __name__ == '__main__':
                    "ORDER BY cities.id ASC")
     states = cursor.fetchall()
     for city in states:
-        print(city)
+        print(city, sep=',', end=' ')
     cursor.close()
     db.close()
