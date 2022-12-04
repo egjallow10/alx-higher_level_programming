8c6a83952d449590aae30c21c2f7595ec92d28bc
egjallo <egjallow10@gmail.com>
2022-12-04 17:45:17 +0000

    fetching data

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This script lists the first State object from database passed into program
+
+Using SQLalchemy, this script connects to a MySQL and
+query a
+"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    a_states = session.query(State) \
+        .order_by(State.id.asc()) \
+        .filter(State.name.ilike('%a%')) \
+        .all()
+
+    for a_state in a_states:
+        print("{}: {}".format(a_state.id, a_state.name))
