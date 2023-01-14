5116fdbb83b8d7ddbe5cb3490c6d66fa52cbe82d
egjallow10 <egjallow10@gmail.com>
2023-01-14 13:03:36 -0800

    post request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #A script to send a  request with a header
-curl -s -H "X-School-User-Id:98" "$1"
+curl -s -H "Content-Type: 
+application/json" -d {"email: test@gmail.com","subject: I will always be here for PLD"} "$1"
