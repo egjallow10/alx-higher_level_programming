6b7316acbdbe07bce0dbd5bd4cbe36163ba880b1
egjallow10 <egjallow10@gmail.com>
2023-01-14 14:46:54 -0800

    let chage|

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #A script to send a  request with a header
-curl -s -X "$1"  -d 'email=test@gmail.com&subject=I will always be here for PLD' POST
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
