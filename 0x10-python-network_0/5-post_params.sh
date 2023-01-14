9bf02c89dd83a588c315ec4ca735ea7421991ebd
egjallow10 <egjallow10@gmail.com>
2023-01-14 13:17:28 -0800

    Post request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #A script to send a  request with a header
-curl -s "$1" -X POST -d 'email=hr@holbertonschool.com' -d 'subject=I will always be here for PLD'
+curl -s "$1" -X POST -d 'email=test@gmail.com' -d 'subject=I will always be here for PLD'
