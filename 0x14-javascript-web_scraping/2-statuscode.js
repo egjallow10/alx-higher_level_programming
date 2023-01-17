aad497d86d9092b77722caa78f2013e8abd0b062
egjallow10 <egjallow10@gmail.com>
2023-01-17 23:52:14 +0000

    fetch

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+request(url, function (err, response, body) {
+  if (err) {
+    console.log('code: ' + response.statusCode);
+  }
+  console.log(JSON.parse(body).title);
+});
