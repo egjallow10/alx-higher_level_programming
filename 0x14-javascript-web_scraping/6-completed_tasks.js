c0eb734755f1139381d86ee66c018fcfd7f46dbc
egjallow10 <egjallow10@gmail.com>
2023-01-18 13:56:47 +0000

    completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 require('request').get(process.argv[2], (err, resp, body) => {
-    if (err) {
-      console.log(err);
-    } else {
-      let tasks = JSON.parse(body);
-      let resp = {};
-      for (let i = 0; i < tasks.length; i++) {
-        if (tasks[i].completed) {
-          if (!(tasks[i].userId in resp)) {
-            resp[tasks[i].userId] = 0;
-          }
-          resp[tasks[i].userId] += 1;
+  if (err) {
+    console.log(err);
+  } else {
+    const tasks = JSON.parse(body);
+    const resp = {};
+    for (let i = 0; i < tasks.length; i++) {
+      if (tasks[i].completed) {
+        if (!(tasks[i].userId in resp)) {
+          resp[tasks[i].userId] = 0;
         }
+        resp[tasks[i].userId] += 1;
       }
-      console.log(resp);
     }
-  });
+    console.log(resp);
+  }
+});
