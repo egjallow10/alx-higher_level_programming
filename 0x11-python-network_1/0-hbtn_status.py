08cbf467edda644e73fc4e7f93895ddeee9fd890
egjallow10 <egjallow10@gmail.com>
2023-01-27 08:26:41 -0800

    respons

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """fetche url and prinf"""
+
 import urllib.request
 
 if __name__ == "__main__":
@@ -7,7 +8,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
-        print("\t- utf8 content:", body.decode())
+        print("    - type:", type(body))
+        print("    - content:", body)
+        print("    - utf8 content:", body.decode())
 
