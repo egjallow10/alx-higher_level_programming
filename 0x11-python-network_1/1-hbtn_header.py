e8111e284a2dc0e35acc9f3a8bdb7b0e222e4dc3
egjallow10 <egjallow10@gmail.com>
2023-01-27 08:36:03 -0800

    header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        headers = response.info()
+        x_request_id = headers.get("X-Request-Id")
+        print("X-Request-Id:", x_request_id)
+
